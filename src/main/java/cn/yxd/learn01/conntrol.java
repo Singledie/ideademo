0c260dd549ef1f5edf0fd15a0ad5e7eb315f80ba
Singledie <58576335+Singledie@users.noreply.github.com>
2020-11-25 09:35:33 +0800

    demo项目提交给git管理

---
+++ src/main/java/cn/yxd/learn01/conntrol.java
@@ -0,0 +1,12 @@
+package cn.yxd.learn01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class conntrol {
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/cn/yxd/learn01/Learn01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yxd.learn01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
